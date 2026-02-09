a8da6f289042112f60dc0364204c51fe71879d60
Jinyang Li <jinyang7@illinois.edu>
2026-02-09 17:54:00 -0600

    WIP: refactor message

--- packages/core/src/acies/core/__init__.py
+++ packages/core/src/acies/core/__init__.py
@@ -1,4 +1,3 @@
-from ._acies_core import __version__
 from .args import common_options
 from .log import init_logger
 from .service import Service, get_zconf, pretty
@@ -7,7 +6,6 @@ from .types import AciesMsg
 # from .msg import Message
 
 __all__ = [
-    '__version__',
     'common_options',
     'get_zconf',
     'init_logger',
