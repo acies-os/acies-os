1711fa721b9a3af56974ce5188f26f5cbff4f354
Jinyang Li <jinyang7@illinois.edu>
2026-03-23 10:13:42 -0500

    Config logger for the library

--- packages/core/src/acies/corev2/__init__.py
+++ packages/core/src/acies/corev2/__init__.py
@@ -1,7 +1,11 @@
+import logging
+
 from .app import AciesApp
 from .context import AciesContext, AppState, TaskState
 from .msg import AciesDelete, AciesGet, AciesHeartbeat, AciesRoute, AciesSet, AciesTensor
 from .task import Job, ScheduleSpec, ServiceSpec, SubscriberSpec
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
     'AciesApp',
