ee7a50e105b94bf8eb4ebd7f7113c13c86fd8b8b
Jinyang Li <jinyang7@illinois.edu>
2026-04-01 12:58:13 -0500

    Add `acies_workers` to control pool size

--- packages/core/src/acies/corev2/app.py
+++ packages/core/src/acies/corev2/app.py
@@ -409,7 +409,7 @@ class AciesApp:
             )
             for task in self._tasks
         }
-        self._executor.start(self.dispatch)
+        self._executor.start(self.dispatch, n_workers=self._app_state.config['sys']['workers'])
         self._router.start(self._executor)
         logger.debug('executor and router started')
 
